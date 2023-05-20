dafad981596f9ff93090c324f6dba9c1368efe88
adrianus123 <adrianussimarmata9@gmail.com>
2023-05-20 12:20:47 +0700

    update

--- Website/bostani_web/resources/views/login.blade.php
+++ Website/bostani_web/resources/views/login.blade.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title }} | Bostani Web</title>
+    <link href="/assets/img/logo_bostani.png" rel="icon" type="text/css">
     @vite('resources/css/app.css')
     @vite('resources/js/script.js')
-
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&display=swap');
   </style>
