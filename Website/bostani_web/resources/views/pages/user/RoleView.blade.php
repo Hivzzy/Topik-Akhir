13b5e09acd8a0a6d25f43bfc1606ca39f66cf509
adrianus123 <adrianussimarmata9@gmail.com>
2023-05-27 19:38:57 +0700

    hapus tambah role

--- Website/bostani_web/resources/views/pages/user/RoleView.blade.php
+++ Website/bostani_web/resources/views/pages/user/RoleView.blade.php
@@ -3,15 +3,6 @@
 @section('container')
     <div class="space-y-4 sm:space-y-6">
         <h1 class="text-lg sm:text-2xl font-semibold">Role User</h1>
-        <div class="bg-white p-4 rounded shadow-md flex space-x-[10px]">
-            <a href="/tambah-role"
-                class="inline-block rounded bg-primary p-2 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-                <div class="flex space-x-1 items-center">
-                    <img src="/assets/icons/add.svg" alt="add icon">
-                    <span class="hidden sm:block">Tambah Data</span>
-                </div>
-            </a>
-        </div>
         <div class="bg-white p-4 space-y-4 rounded shadow-md">
             <div class="flex flex-col">
                 <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -22,7 +13,6 @@
                                     <tr>
                                         <th>No</th>
                                         <th>Nama Role</th>
-                                        <th>Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -30,12 +20,6 @@
                                         <tr class="border-b">
                                             <td class="whitespace-nowrap px-6 py-4">{{ $loop->iteration }}</td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $role->role_name }}</td>
-                                            <td>
-                                                <a href="#"
-                                                    class="inline-block whitespace-nowrap rounded-[0.27rem] bg-danger-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-danger-700">
-                                                    Hapus
-                                                </a>
-                                            </td>
                                         </tr>
                                     @endforeach
                                 </tbody>
