fd1c9198660727d38f05595fa2cc429f9491b385
yosualr <yosualr12@gmail.com>
2023-05-29 13:06:47 +0700

    Update error carbon package
    Install dulu bre carbon nya buat bisa nyesuain kayak jam indo pake composer, ada da tutor nya di ch*pgpt

--- Website/bostani_web/app/Providers/AppServiceProvider.php
+++ Website/bostani_web/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         config(['app.locale'=>'id']);
-        Carbon:setLocale('id');
+        Carbon::setLocale('id');
     }
 }
