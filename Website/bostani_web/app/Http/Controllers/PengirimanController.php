9f5f9af7ff97ff1db7de5674809fc5da6e8cd40c
yosualr <yosualr12@gmail.com>
2023-05-30 17:05:23 +0700

    Update PengirimanController dan PengirimanModel

--- Website/bostani_web/app/Http/Controllers/PengirimanController.php
+++ Website/bostani_web/app/Http/Controllers/PengirimanController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PengirimanModel;
+use App\Models\DriverModel;
+use App\Models\PesananModel;
+use App\Models\StatusPengirimanModel;
 use Illuminate\Http\Request;
 
 class PengirimanController extends Controller
 {
     public function index()
     {
+        $title = 'Hapus data Pengiriman!';
+        $text = "Anda yakin ingin menghapus data pengiriman?";
+        confirmDelete($title, $text);
+
+        $pengiriman = new PengirimanModel();
+        $data = $pengiriman->getPengiriman();
+
         return view('pages.pengiriman.PengirimanView', [
             'title' => 'Data Pengiriman',
-            'active' => 'send-item',
+            'active' => 'deliveries',
+            'pengirimans' => $data,
         ]);
     }
 }
+?>
